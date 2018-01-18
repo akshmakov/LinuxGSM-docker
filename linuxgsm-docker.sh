422c6b2813c5093eabaeb627b3c6a87cdd9e3566
jkljkl1197 <jordan1197@hotmail.ca>
2018-01-18 09:56:43 -0500

    add the case to a function; need to be test

--- linuxgsm-docker.sh
+++ linuxgsm-docker.sh
@@ -14,6 +14,77 @@ Path=`pwd`
 ## check if the container already running (true or '')
 status=$(sudo docker inspect --format="{{.State.Running}}" $InstanceName 2> /dev/null)
 
+fn_command_sender(){
+
+	case $cmd in
+		"install")
+		    read -a type
+		    sudo docker exec $InstanceName $ServerType install $type
+		    ;;
+
+		"start")
+		    sudo docker exec $InstanceName $ServerType start
+		    sleep 2
+		    sudo docker exec $InstanceName alert_discord.sh start
+		    ;;
+
+		"stop")
+		    if [ "$status" == "true" ]
+		    then
+			sudo docker exec $InstanceName alert_discord.sh stop
+			sudo docker exec $InstanceName $ServerType stop
+			sudo docker kill $InstanceName
+		    fi
+		    ;;
+
+		"restart")
+		    sudo docker exec $InstanceName alert_discord.sh restart
+		    sudo docker exec $InstanceName $ServerType restart
+		    ;;
+
+		"console")
+		    sudo docker exec $InstanceName $ServerType console
+		    ;;
+
+		"monitor")
+		    sudo docker exec $InstanceName $ServerType monitor
+		    ;;
+
+		"update") ## update stop the server if is already running(lgsm script).
+		    sudo docker exec $InstanceName alert_discord.sh update
+		    sudo docker exec $InstanceName $ServerType update
+		    ;;
+
+		"backup")
+		    sudo docker exec $InstanceName $ServerType backup
+		    ;;
+
+		"install")
+		    sudo docker exec $InstanceName $ServerType install $2
+		    ;;
+
+		#"conjob") ## as been tested but need a custom path ...
+		#    need to find a way to add this line in crontab of the main linux "* */3 * * * bash ${Path}/linuxgsm-docker.sh command bash check_version.sh >/dev/null 2>&1"
+		#    ;;
+
+		"attach")
+		    echo "dettach with ctrl+p & ctrl+q"
+		    sudo docker attach $InstanceName
+		    ;;
+
+		"command")
+		    ## Need to be test (take all parameter after the first one)
+		    sudo docker exec -it $InstanceName ${@:2}
+		    ;;
+
+		*)
+		    exit 1
+
+	esac
+
+}
+
+
 if [ "$status" != "true" ] && [ "$1" != "stop" ]
 then
 	echo "docker container was not running. start it for you."
@@ -29,76 +100,11 @@ fi
 if [ "$1" != "" ]
 then
        	cmd=$1
+	fn_command_sender $cmd
 else
 	echo $"Usage: $0 {start|stop|restart|console|monitor|update|backup|attach|command|install}"
 	read -a cmd
+	fn_command_sender $cmd
 fi
-
-
-case $cmd in
-        "install")
-            read -a type
-	    sudo docker exec $InstanceName $ServerType install $type
-            ;;
-
-        "start")
-            sudo docker exec $InstanceName $ServerType start
-	    sleep 2
-	    sudo docker exec $InstanceName alert_discord.sh start
-            ;;
-
-        "stop")
-	    if [ "$status" == "true" ]
-	    then
-	        sudo docker exec $InstanceName alert_discord.sh stop
-            	sudo docker exec $InstanceName $ServerType stop
-	    	sudo docker kill $InstanceName
-	    fi
-            ;;
-
-        "restart")
-	    sudo docker exec $InstanceName alert_discord.sh restart
-            sudo docker exec $InstanceName $ServerType restart
-            ;;
-
-        "console")
-            sudo docker exec $InstanceName $ServerType console
-            ;;
-
-        "monitor")
-            sudo docker exec $InstanceName $ServerType monitor
-            ;;
-
-        "update") ## update stop the server if is already running(lgsm script).
-	    sudo docker exec $InstanceName alert_discord.sh update
-            sudo docker exec $InstanceName $ServerType update
-            ;;
-
-        "backup")
-            sudo docker exec $InstanceName $ServerType backup
-            ;;
-	    
-	"install")
-            sudo docker exec $InstanceName $ServerType install $2
-            ;;
-
-        #"conjob") ## as been tested but need a custom path ...
-	#    need to find a way to add this line in crontab of the main linux "* */3 * * * bash ${Path}/linuxgsm-docker.sh command bash check_version.sh >/dev/null 2>&1"
-        #    ;;
-
-	"attach")
-	    echo "dettach with ctrl+p & ctrl+q"
-	    sudo docker attach $InstanceName
-	    ;;
-
-	"command")
-	    ## Need to be test (take all parameter after the first one)
-            sudo docker exec -it $InstanceName ${@:2}
-	    ;;
-
-        *)
-            exit 1
-
-esac
 
 #sudo docker run --name arkserver --rm -it -d -v "/home/lgsm/:/home/lgsm" lgsm-docker bash $@
