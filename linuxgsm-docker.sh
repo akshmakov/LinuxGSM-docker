c9f8002deda520494833116d4ebb6f7e3c778945
jkljkl1197 <jordan1197@hotmail.ca>
2018-01-18 14:19:21 -0500

    better Shell setup ... cleaning

--- linuxgsm-docker.sh
+++ linuxgsm-docker.sh
@@ -11,18 +11,22 @@ Img='lgsm-docker'
 ## current path; plz execute this script from it folder
 Path=`pwd`
 ## Set the Network Used by docker
-network='host'
+Network='host'
 ## Set the hostname for the docker container
-hostname='LGSM'
+Hostname='LGSM'
 ## Set it to False if you don't have a discord custom script like me
-enable="True"
+DiscordNotifier="True"
 
 ## check if the container already running (true or '')
 status=$(sudo docker inspect --format="{{.State.Running}}" $InstanceName 2> /dev/null)
 
 fn_discord_custom_sender(){
 
-	
+	if [ "${DiscordNotifier}" == "true" ]
+	then
+		sleep 2
+		sudo docker exec ${InstanceName} alert_discord.sh ${cmd}
+	fi
 
 }
 
@@ -33,52 +37,51 @@ fn_command_sender(){
 		    if[ $2 != "" ]
 		    then
 			read -a type
-			sudo docker exec $InstanceName $ServerType install $type
+			sudo docker exec ${InstanceName} ${ServerType} install ${type}
 		    else
-		    	echo "Missing parameter for the serveur name to install"
-		    	sudo docker exec $InstanceName $ServerType install
+		    	echo "Missing parameter for the serveur name to install, showing server game list"
+		    	sudo docker exec ${InstanceName} ${ServerType} install
 		    fi
 		    ;;
 
 		"start")
-		    sudo docker exec $InstanceName $ServerType start
-		    sleep 2
-		    sudo docker exec $InstanceName alert_discord.sh start
+		    sudo docker exec ${InstanceName} ${ServerType} start
+		    fn_discord_custom_sender ${cmd}
 		    ;;
 
 		"stop")
 		    if [ "$status" == "true" ]
 		    then
-			sudo docker exec $InstanceName alert_discord.sh stop
-			sudo docker exec $InstanceName $ServerType stop
-			sudo docker kill $InstanceName
+			sudo docker exec ${InstanceName} ${ServerType} stop
+			fn_discord_custom_sender ${cmd}
+			sudo docker kill ${InstanceName}
 		    fi
 		    ;;
 
 		"restart")
-		    sudo docker exec $InstanceName alert_discord.sh restart
-		    sudo docker exec $InstanceName $ServerType restart
+		    sudo docker exec ${InstanceName} ${ServerType} restart
+		    fn_discord_custom_sender ${cmd}
+		    ;;
+		    
+		"update") ## update stop the server if is already running(lgsm script).
+		    sudo docker exec ${InstanceName} ${ServerType} update
+		    fn_discord_custom_sender ${cmd}
 		    ;;
 
 		"console")
-		    sudo docker exec $InstanceName $ServerType console
+		    sudo docker exec ${InstanceName} ${ServerType} console
 		    ;;
 
 		"monitor")
-		    sudo docker exec $InstanceName $ServerType monitor
-		    ;;
-
-		"update") ## update stop the server if is already running(lgsm script).
-		    sudo docker exec $InstanceName alert_discord.sh update
-		    sudo docker exec $InstanceName $ServerType update
+		    sudo docker exec ${InstanceName} ${ServerType} monitor
 		    ;;
 		    
 		"validate")
-		    sudo docker exec $InstanceName $ServerType validate
+		    sudo docker exec ${InstanceName} ${ServerType} validate
 		    ;;
 
 		"backup")
-		    sudo docker exec $InstanceName $ServerType backup
+		    sudo docker exec ${InstanceName} ${ServerType} backup
 		    ;;
 
 		"conjob")
@@ -90,12 +93,12 @@ fn_command_sender(){
 
 		"attach")
 		    echo "dettach with ctrl+p & ctrl+q"
-		    sudo docker attach $InstanceName
+		    sudo docker attach ${InstanceName}
 		    ;;
 
 		"command")
 		    ## Need to be test (take all parameter after the first one)
-		    sudo docker exec -it $InstanceName ${@:2}
+		    sudo docker exec -it ${InstanceName} ${@:2}
 		    ;;
 
 		*)
@@ -106,12 +109,12 @@ fn_command_sender(){
 }
 
 
-if [ "$status" != "true" ] && [ "$1" != "stop" ]
+if [ "${status}" != "true" ] && [ "$1" != "stop" ]
 then
 	echo "docker container was not running. start it for you."
-	sudo docker rm $InstanceName 2> /dev/null
-	sudo docker run --name $InstanceName --restart always --net=${network} --hostname ${hostname} -it -d -v "/home/lgsm/:/home/lgsm" $Img bash 2> /dev/null
-elif [ "$status" == "true" ]
+	sudo docker rm ${InstanceName} 2> /dev/null
+	sudo docker run --name ${InstanceName} --restart always --net=${Network} --hostname ${Hostname} -it -d -v "/home/lgsm/:/home/lgsm" ${Img} bash 2> /dev/null
+elif [ "${status}" == "true" ]
 then
 	echo "docker container already running, append command."
 else
@@ -122,11 +125,11 @@ fi
 if [ "${#}" > 0 ]
 then
        	cmd=$1
-	fn_command_sender $cmd
+	fn_command_sender ${cmd}
 else
 	echo $"Usage: $0 {start|stop|restart|console|monitor|update|backup|attach|command|install}"
 	read -a cmd
-	fn_command_sender $cmd
+	fn_command_sender ${cmd}
 fi
 
 #sudo docker run --name arkserver --rm -it -d -v "/home/lgsm/:/home/lgsm" lgsm-docker bash $@
